2d4a9fd01a6bc7b51c475bad1452e3e51f672673
yaaHosein <layali.14.peshawar@hotmail.com>
2020-06-17 20:20:15 +0430

    calculator JS codes

---
+++ calculator.js
@@ -0,0 +1,12 @@
+<script>
+        class Calculater {
+            add(number1, number2) {
+                console.log(number1 + number2);
+
+            };
+
+            subtract(number1, number2) {
+                console.log(number2 - number1)
+            };
+        };
+    </script>
