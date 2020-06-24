091be0b7409f21c7c13ffa8d5c878b3ab7d97316
yaaHosein <layali.14.peshawar@hotmail.com>
2020-06-24 18:48:18 +0430

    calculator html after adding the link to JS

--- calculator.js
+++ calculator.js
@@ -1,12 +1,10 @@
-<script>
-        class Calculater {
-            add(number1, number2) {
-                console.log(number1 + number2);
+class Calculater {
+    add(number1, number2) {
+        console.log(number1 + number2);
 
-            };
+    };
 
-            subtract(number1, number2) {
-                console.log(number2 - number1)
-            };
-        };
-    </script>
+    subtract(number1, number2) {
+        console.log(number2 - number1)
+    };
+};
