ff852aed691c1868f1b3faf652e59cee69164a6c
yaaHosein <layali.14.peshawar@hotmail.com>
2020-07-25 19:46:52 +0430

    not completed yet

--- calculator.js
+++ calculator.js
@@ -1,11 +1,41 @@
-class Calculater {
-    add(number1, number2) {
-        console.log(number1 + number2);
+const app0 = document.getElementById("app0");
+app0.addEventListener("click", function() {
+    console.log("0")
+});
 
-    };
-
-    subtract(number1, number2) {
-        console.log(number2 - number1)
-    };
-};
-//.
+const app1 = document.getElementById("app1");
+app1.addEventListener("click", function() {
+    console.log("1")
+});
+const app2 = document.getElementById("app2");
+app2.addEventListener("click", function() {
+    console.log("2")
+});
+const app3 = document.getElementById("app3");
+app3.addEventListener("click", function() {
+    console.log("3")
+});
+const app4 = document.getElementById("app4");
+app4.addEventListener("click", function() {
+    console.log("4")
+});
+const app5 = document.getElementById("app5");
+app5.addEventListener("click", function() {
+    console.log("5")
+});
+const app6 = document.getElementById("app6");
+app6.addEventListener("click", function() {
+    console.log("6")
+});
+const app7 = document.getElementById("app7");
+app7.addEventListener("click", function() {
+    console.log("7")
+});
+const app8 = document.getElementById("app8");
+app8.addEventListener("click", function() {
+    console.log("8")
+});
+const app9 = document.getElementById("app9");
+app9.addEventListener("click", function() {
+    console.log("9")
+});
