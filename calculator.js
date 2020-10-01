3e4af4005b05726fe281885d9e5fa310ddd9d349
yaaHosein <layali.14.peshawar@hotmail.com>
2020-10-01 19:21:34 +0330

    some updtes

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const app0 = document.getElementById("app0");
 app0.addEventListener("click", function() {
     console.log("0")
 });
-
+document.getElementById('app0').innerHTML = "0"
 const app1 = document.getElementById("app1");
 app1.addEventListener("click", function() {
     console.log("1")
@@ -31,6 +31,7 @@ const app7 = document.getElementById("app7");
 app7.addEventListener("click", function() {
     console.log("7")
 });
+
 const app8 = document.getElementById("app8");
 app8.addEventListener("click", function() {
     console.log("8")
@@ -38,4 +39,14 @@ app8.addEventListener("click", function() {
 const app9 = document.getElementById("app9");
 app9.addEventListener("click", function() {
     console.log("9")
+});
+
+
+var result = 0;
+var increaseButton = document.getElementById("increase-button");
+var result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
 });
