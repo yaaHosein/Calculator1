0ca999bafef0ed01ee38dd823ed7ad8bc9e627dd
yaaHosein <layali.14.peshawar@hotmail.com>
2020-10-05 13:15:24 +0330

    today's updtes

--- calculator.js
+++ calculator.js
@@ -2,62 +2,131 @@ const button0 = document.getElementById("button0");
 button0.addEventListener("click", function() {
     console.log("0")
 });
+let result = 0;
+let increaseButton = document.getElementById("button0");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
+
 
 document.getElementById('button0').innerHTML = "0"
 const button1 = document.getElementById("button1");
 button1.addEventListener("click", function() {
     console.log("1")
 });
-// let count = 0;
-// let result = document.getElementById("result1");
-// result1.textContent = count;
-// increaseButton.addEventListener("click", function() {
-//     count++;
-//     result1.textContent = count;
-// });
+
+let increaseButton = document.getElementById("button1");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
 
 const button2 = document.getElementById("button2");
-
 button2.addEventListener("click", function() {
     console.log("2")
 });
 
-{ document.getElementById("button2").innerHTML = 2; }
+let increaseButton = document.getElementById("button2");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
 
 const button3 = document.getElementById("button3");
 button3.addEventListener("click", function() {
     console.log("3")
 });
+
+let increaseButton = document.getElementById("button3");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
+
 const button4 = document.getElementById("button4");
 button4.addEventListener("click", function() {
     console.log("4")
 });
+
+let increaseButton = document.getElementById("button4");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
+
 const button5 = document.getElementById("button5");
 button5.addEventListener("click", function() {
     console.log("5")
 });
+
+let increaseButton = document.getElementById("button5");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
+
 const button6 = document.getElementById("button6");
 button6.addEventListener("click", function() {
     console.log("6")
 });
+
+let increaseButton = document.getElementById("button6");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
+
 const button7 = document.getElementById("button7");
 button7.addEventListener("click", function() {
     console.log("7")
 });
 
+let increaseButton = document.getElementById("button7");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
+
+
 const button8 = document.getElementById("button8");
 button8.addEventListener("click", function() {
     console.log("8")
 });
+
+let increaseButton = document.getElementById("button8");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
+
 const button9 = document.getElementById("button9");
 button9.addEventListener("click", function() {
     console.log("9")
 });
 
-//const result = document.getElementById("increase-button");
-// console.log(result.innerHTML);
-//
-//................................
-//let result = div.innerHTML
-//div.innerHTML = "7";
-//document.getElementById("button7").innerHTML = "7";
+let increaseButton = document.getElementById("button9");
+let result1 = document.getElementById("result1");
+result1.textContent = result;
+increaseButton.addEventListener("click", function() {
+    result++;
+    result1.textContent = result;
+})
