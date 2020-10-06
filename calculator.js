20e3e2939478582f7410648ea0433660d709fdaf
yaaHosein <layali.14.peshawar@hotmail.com>
2020-10-06 15:52:52 +0330

    after removing the duplicated codes and changing result1.textContent = result; to result1.textContent = count;

--- calculator.js
+++ calculator.js
@@ -1,128 +1,57 @@
+document.getElementById('button0').innerHTML = "0"
+let button = document.getElementById("increase-button");
+
+let count = 0;
+let increaseButton = document.getElementById("button0");
+result1.textContent = count;
+
+
 const button0 = document.getElementById("button0");
 button0.addEventListener("click", function() {
-
+    result1.textContent = 0;
 });
-let result = 0;
-let increaseButton = document.getElementById("button0");
 
-document.getElementById('button0').innerHTML = "0"
 const button1 = document.getElementById("button1");
-button1.addEventListener("click", function() {});
-
-let result1 = document.getElementById("result1");
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
-
+button1.addEventListener("click", function() {
+    result1.textContent = 1;
+});
 
 const button2 = document.getElementById("button2");
 button2.addEventListener("click", function() {
-
+    result1.textContent = 2;
 });
-
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
 
 const button3 = document.getElementById("button3");
 button3.addEventListener("click", function() {
-
+    result1.textContent = 3;
 });
-
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
 
 const button4 = document.getElementById("button4");
 button4.addEventListener("click", function() {
-
+    result1.textContent = 4;
 });
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
 
 const button5 = document.getElementById("button5");
 button5.addEventListener("click", function() {
-
+    result1.textContent = 5;
 });
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
 
 const button6 = document.getElementById("button6");
 button6.addEventListener("click", function() {
-
+    result1.textContent = 6;
 });
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
 
 const button7 = document.getElementById("button7");
 button7.addEventListener("click", function() {
-
+    result1.textContent = 7;
 });
-
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
-
 
 const button8 = document.getElementById("button8");
 button8.addEventListener("click", function() {
-
+    result1.textContent = 8;
 });
-
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
 
 const button9 = document.getElementById("button9");
 button9.addEventListener("click", function() {
-
+    result1.textContent = 9;
 });
-
-
-
-result1.textContent = result;
-increaseButton.addEventListener("click", function() {
-    result++;
-    result1.textContent = result;
-})
-
-let button = document.getElementById("increase-button");
