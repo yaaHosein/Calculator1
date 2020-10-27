bd58d4100adf23e1938790c0a1ed273d5dcf2910
yaaHosein <layali.14.peshawar@hotmail.com>
2020-10-27 19:56:19 +0330

    Activating buttons 0,2,3,4,5,6,7,8,9

--- calculator.js
+++ calculator.js
@@ -1,61 +1,88 @@
 document.getElementById('button0').innerHTML = "0";
 let button = document.getElementById("result1");
 
-const button0 = document.getElementById("button0");
+
 button0.addEventListener("click", function() {
-    result1.textContent = 0;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 0;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
 
-
-const button1 = document.getElementById("button1");
 button1.addEventListener("click", function() {
     const theCurrentValueOfResult1 = result1.textContent;
     const newValue = theCurrentValueOfResult1 + 1;
     console.log(parseInt(newValue));
     result1.textContent = newValue;
 
-
 });
 
 
-const button2 = document.getElementById("button2");
 button2.addEventListener("click", function() {
-    result1.textContent = 2;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 2;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
 
 });
 
-const button3 = document.getElementById("button3");
+
 button3.addEventListener("click", function() {
-    result1.textContent = 3;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 3;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
 
 
-const button4 = document.getElementById("button4");
 button4.addEventListener("click", function() {
-    result1.textContent = 4;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 4;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
 
-const button5 = document.getElementById("button5");
+
 button5.addEventListener("click", function() {
-    result1.textContent = 5;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 5;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
 
-const button6 = document.getElementById("button6");
 button6.addEventListener("click", function() {
-    result1.textContent = 6;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 6;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
 
-const button7 = document.getElementById("button7");
 button7.addEventListener("click", function() {
-    result1.textContent = 7;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 7;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
 
-const button8 = document.getElementById("button8");
 button8.addEventListener("click", function() {
-    result1.textContent = 8;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 8;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
 
-const button9 = document.getElementById("button9");
+
 button9.addEventListener("click", function() {
-    result1.textContent = 9;
+    const theCurrentValueOfResult1 = result1.textContent;
+    const newValue = theCurrentValueOfResult1 + 9;
+    console.log(parseInt(newValue));
+    result1.textContent = newValue;
+
 });
