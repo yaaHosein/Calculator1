252b1cf74493ab6f2a9489d653c1ba8a56b5bc27
yaaHosein <layali.14.peshawar@hotmail.com>
2020-11-24 21:43:39 +0330

    trying to prevent the decimal point redublicate itself

--- calculator.js
+++ calculator.js
@@ -89,76 +89,17 @@ button9.addEventListener("click", function() {
 });
 
 periodButton.addEventListener("click", function() {
-    ////%% here I am listening 👌👂 the period-button click 
-    // let __result;
-    //&& here I am saving the result in a vaiarble 
     const currentValueOfResult = result.textContent;
-    ////&& here I am getting the value of result div and saving  it in a variable 
-    // if (currentValueOfResult.includes('.' + '.')) { result.textContent == "." } else { result.textContent == "" };
-    //&& here i am checking using if statment that if there is double "." in the current value of result div so let it to become only one period otherwise display in the result div empty  
     const newValue = currentValueOfResult + ".";
-    //&& here I'm adding "." to the current value of result div and saving it in another variable 
-    // if (currentValueOfResult.includes('.' + '.')) { result.textContent == "." }
-    //&& here i am checking using if statment that if there is double "." in the current value of result div so let it to become only one period
-    // if (currentValueOfResult.includes(".")) {result.textContent == currentValueOfResult + "."}
-    // && here i am checking using if statment that if there is one . in the current Value Of Result div let the result div equal to one .
     result.textContent = newValue;
-    //&& here i am displaying the last value to result div 
-    // if (newValue.includes('.') = false) { result.textContent == currentValueOfResult + "." }
-    // && here i am checking using if statment that if there is one . in the new Value Of Result div let rsult div ==  the current value of result div +  .
-    // if (currentValueOfResult.includes('.') == false) { result.textContent = currentValueOfResult + "." }
-    // && here i am checking using if statment that if there is not  . in the current Value Of Result div let the result div equal to current value of result div + one .
-    // if (currentValueOfResult.includes('.') == false) { result.textContent = newValue + "." }
-    //&& // && here i am checking using if statment that if there is not . in the current Value Of Result div let the result div equal to new value + one .
-    // if (currentValueOfResult.includes('.') == false) { result.textContent = currentValueOfResult + "." }
-    // && here i am checking using if statment that if there is not  . in the current Value Of Result div let the result div equal to current value of result div + one .
-
-    // if (currentValueOfResult.includes('.') == false) { result.textContent = newValue + "." }
-    // && here i am checking using if statment that if there is not  . in the current Value Of Result div let the result div equal to new value of result div + one .
-
-    // if (currentValueOfResult.includes('.') = false) { result.textContent == newValue + "." }
-    // && here i am checking using if statment that if there is not  . in the current Value Of Result div let the result div equal to current value of result div + one .
-
-    //else { result.textContent == "" };
-    //&& i think this comment is related to one of the first tries to find the solutions 
-
-    // var __result = newValue;
-    //$$ here i am saving the result div in a vairable called new Value ;
-    // __result.includes("."++);
-    //&& here i am checking if this result has 2 and more "."
-    // console.log("");
-    //&& here i ma copying the result to console panel 
+    if (currentValueOfResult.includes('.') === false) { result.textContent = currentValueOfResult + "." } else { result.textContent = currentValueOfResult }
 });
-
-// var input = '';
-// $(':button').click(function() {
-//     input = $(this).attr("value");
-//     if (input === '.') {
-//         if (input === '.' + '.') {
-//             input = '';
-//         }
-//     }
-// });
-// ========================
-// function update_input() {
-
-//     input_1 = input1.text;
-
-//     if (input_1.match(".") == false) {
-//         input1.restrict = ".0-9";
-//     } else {
-//         input1.restrict = "0-9";
-//     }
-// // }
-// ===
-// === === === === === === ===
 
 addingButton.addEventListener("click", function() {
     operator = "+";
     lastValue = result.textContent;
     result.textContent = "";
 });
-
 subtractingButton.addEventListener("click", function() {
     operator = "-";
     lastValue = result.textContent;
@@ -181,17 +122,17 @@ equalButton.addEventListener("click", function() {
     let secondOperand = parseFloat(result.textContent);
     let _result;
     console.log("opertator", operator);
-    if (operator === "+") {
+    if (operator === "+")
         _result = firstOperand + secondOperand;
-    }
-    if (operator === "-") {
+
+    if (operator === "-")
         _result = firstOperand - secondOperand;
-    }
-    if (operator === "*") {
+
+    if (operator === "*")
         _result = firstOperand * secondOperand;
-    }
-    if (operator === "/") {
+
+    if (operator === "/")
         _result = firstOperand / secondOperand;
-    }
+
     result.textContent = _result;
 });
