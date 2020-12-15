1074bc1fb0ed9f0ea3fb1bfc81e89ddbd358931b
yaaHosein <layali.14.peshawar@hotmail.com>
2020-12-15 17:06:15 +0330

    making the number do not disappear after clicking on the opertors

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 let lastValue;
 let operator;
-let _firstOperand;
+let _firstOperand = "+" || "-" || "*" || "/";
 
 let button = document.getElementById("result");
 
@@ -11,83 +11,83 @@ clearingButton.addEventListener("click", function() {
 });
 
 button0.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 0;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button1.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 1;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button2.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 2;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button3.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 3;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button4.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 4;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button5.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 5;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button6.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 6;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button7.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 7;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button8.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 8;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 button9.addEventListener("click", function() {
+    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
     const currentValueOfResult = result.textContent;
     const newValue = currentValueOfResult + 9;
     console.log(parseInt(newValue));
     result.textContent = newValue;
-    if (_firstOperand === "+" || "-" || "*" || "/") result.textContent = "";
 });
 
 periodButton.addEventListener("click", function() {
@@ -99,7 +99,7 @@ periodButton.addEventListener("click", function() {
 negativeValueButton.addEventListener("click", function() {
     const currentValueOfResult = result.textContent;
     if (currentValueOfResult !== "" && currentValueOfResult[0] !== "-") { result.textContent = "-" + currentValueOfResult };
-    if (currentValueOfResult !== "" && currentValueOfResult[0] === "-") { result.textContent = currentValueOfResult.slice(0) };
+    if (currentValueOfResult !== "" && currentValueOfResult[0] === "-") { result.textContent = currentValueOfResult.slice(1) };
 });
 
 addingButton.addEventListener("click", function() {
