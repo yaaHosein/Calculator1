28d3f41e8f1876c97c2d23d0d15ec8c6f05dc106
yaaHosein <layali.14.peshawar@hotmail.com>
2021-01-28 22:21:35 +0330

    adding two vaiables numBtns and operBtns

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,6 +1,9 @@
-class CalcualtorLogic {
-    add(number1, number2) { number1 + number2 };
-    subtract(number1, number2) { number1 - number2 };
-    divide(number1, number2) { number1 / number2 };
-    multiply(number1, number2) { number1 * number2 };
-};
+ let numBtns = document.querySelectorAll("[data-number]")
+ let operBtns = document.querySelectorAll("[data-operator]")
+
+ class CalcualtorLogic {
+     add(number1, number2) { number1 + number2 };
+     subtract(number1, number2) { number1 - number2 };
+     divide(number1, number2) { number1 / number2 };
+     multiply(number1, number2) { number1 * number2 };
+ };
