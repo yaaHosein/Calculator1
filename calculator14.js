2f0a27a0aefdd395913c5bdac1446d7e0c62997c
yaaHosein <layali.14.peshawar@hotmail.com>
2021-01-28 16:08:39 +0330

    creating new Js file caled claculator14 and creating a class in it and adding the main 4 caclulating methods in it and adding two parameters and giving setting the role of each method in it

---
+++ calculator14.js
@@ -0,0 +1,11 @@
+class CalcualtorLogic {
+    add(number1, number2)
+    subtract(number1, number2)
+    divide(number1, number2)
+    multiply(number1, number2)
+
+    add() { number1 + number2 };
+    subtract() { number1 - number };
+    divide() { number1 / number2 };
+    multiply() { number1 * number2 };
+}
