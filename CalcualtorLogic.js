be4882f5e0229d4893406720c11a5699591f378d
yaaHosein <layali.14.peshawar@hotmail.com>
2021-01-28 21:52:28 +0330

    renaming the new JS file and fixing the adding it to index.html and removing the doublicated methods

---
+++ CalcualtorLogic.js
@@ -0,0 +1,6 @@
+class CalcualtorLogic {
+    add(number1, number2) { number1 + number2 };
+    subtract(number1, number2) { number1 - number2 };
+    divide(number1, number2) { number1 / number2 };
+    multiply(number1, number2) { number1 * number2 };
+};
