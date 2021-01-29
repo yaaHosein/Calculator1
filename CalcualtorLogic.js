fb6652e5fadae7373c8662664c03893f2d50f948
yaaHosein <layali.14.peshawar@hotmail.com>
2021-01-29 03:36:37 +0330

    trying to use the class to do mathimatical calculations by using this keyword

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,9 +1,16 @@
- let numBtns = document.querySelectorAll("[data-number]")
- let operBtns = document.querySelectorAll("[data-operator]")
-
- class CalcualtorLogic {
-     add(number1, number2) { number1 + number2 };
-     subtract(number1, number2) { number1 - number2 };
-     divide(number1, number2) { number1 / number2 };
-     multiply(number1, number2) { number1 * number2 };
- };
+class CalcualtorLogic {
+    add(number1, number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+    calculate() {
+        console.log(this.number1 + this.number2)
+    }
+    subtract(number1, number2) { number1 - number2 };
+    divide(number1, number2) { number1 / number2 };
+    multiply(number1, number2) { number1 * number2 };
+};
+let firstCalculation = new CalcualtorLogic(1, 2)
+    // let firstCalculation = new CalcualtorLogic('1', '2')
+firstCalculation.calculate();
+console.log(parseInt(firstCalculation));
