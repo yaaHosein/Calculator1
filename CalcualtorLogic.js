7857513aa0c5a938d013d7580f0c47ecbf0a639b
yaaHosein <layali.14.peshawar@hotmail.com>
2021-01-29 14:16:08 +0330

    removing the constructor method and adding return to the add method

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,23 +1,23 @@
-class CalcualtorLogic {
-    constructor(number1, number2) {
-        this.number1 = number1;
-        this.number2 = number2;
-    }
-    add() {
-        console.log(this.number1 + this.number2)
+class CalcualtorLogic(number1, number2) {
+
+    this.number1 = number1;
+    this.number2 = number2;
+
+    add(number1, number2) {
+        return (this.number1 + this.number2)
     };
-    subtract() {
-        console.log(this.number1 - this.number2)
-    };
-    divide() {
-        console.log(this.number1 / this.number2)
-    }
-    multiply() {
-        console.log(this.number1 * this.number2)
-    };
+    // subtract(number1, number2) {
+    //     return (this.number1 - this.number2)
+    // };
+    // divide(number1, number2) {
+    //     return (this.number1 / this.number2)
+    // }
+    // multiply(number1, number2) {
+    //     return (this.number1 * this.number2)
+    // };
 };
-let firstCalculation = new CalcualtorLogic(1, 2)
-firstCalculation.add();
-firstCalculation.subtract();
-firstCalculation.divide();
-firstCalculation.multiply();
+let firstCalculation = new CalcualtorLogic(number1, number2)
+firstCalculation.add(1, 2);
+// firstCalculation.subtract();
+// firstCalculation.divide();
+// firstCalculation.multiply();
