cb32823104d2ff1f7c96f38494dbf9972e4a456d
yaaHosein <layali.14.peshawar@hotmail.com>
2021-01-31 03:13:34 +0330

    logging firstCalculation's value

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,13 +1,13 @@
 class CalcualtorLogic {
     add(number1, number2) {
-        let firstCalculation = number1 + number2
-            // let firstCalculation = number1 + number2;
+        let firstCalculation = number1 + number2;
         return firstCalculation;
+        // return firstCalculation.add(number1, number2);
     };
 };
-let firstCalculation_ = new CalcualtorLogic(1, 2)
-firstCalculation_.add(1, 2);
-// firstCalculation.add(1, 2);
+// let firstCalculation_ = new CalcualtorLogic(1, 7)
+firstCalculation.add(1, 2);
+console.log(firstCalculation.add(1, 2))
 
 
 // firstCalculation.subtract();
