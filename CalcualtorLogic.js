672a117f3fa6cc011d07571684a846f500b701cc
yaaHosein <layali.14.peshawar@hotmail.com>
2021-01-31 13:37:11 +0330

    trying to acitivate the add method by defining 3 var number1 and number2 and resul and returning the result and adding .add() to it

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,14 +1,43 @@
 class CalcualtorLogic {
     add(number1, number2) {
-        let firstCalculation = number1 + number2;
-        return firstCalculation;
-        // return firstCalculation.add(number1, number2);
+        let result = number1 + number2;
+        return result;
     };
 };
-// let firstCalculation_ = new CalcualtorLogic(1, 7)
-firstCalculation.add(1, 2);
-console.log(firstCalculation.add(1, 2))
+let number1 = 1;
+let number2 = 2;
 
+let result = 10;
+
+result.add(1, 2);
+console.log(result.add(1, 2))
+
+
+
+
+// function sum(num1, num2) {
+//     var result = num1 + num2
+//     return result
+// }
+// var number1 = 5;
+// var number2 = 10;
+
+// var result1 = sum(number1, number2)
+// console.log(result1)
+
+
+// class CalcualtorLogic {
+
+//     add(number1, number2) {
+
+//         return firstCalculation = number1 + number2;
+//         // return firstCalculation.add(number1, number2);
+//     };
+// };
+// let firstCalculation;
+// // let firstCalculation_ = new CalcualtorLogic(1, 7)
+// firstCalculation.add(1, 2);
+// console.log(firstCalculation.add(1, 2))
 
 // firstCalculation.subtract();
 // firstCalculation.divide();
