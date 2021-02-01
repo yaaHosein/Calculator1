ce1dad345e27cd728cfb3847ac7f077564f3bf34
yaaHosein <layali.14.peshawar@hotmail.com>
2021-02-01 16:29:05 +0330

    trying to make add method add two numbers by new class variable called calculatorLogic_

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,50 +1,18 @@
 let result = 14;
+
 class CalcualtorLogic {
     add(number1, number2) {
-        result = number1 + number2;
+        let result = number1 + number2;
+        // result = number1 + number2;
         return result;
+        // return result = number1 + number2;
     };
 };
-result.add(1, 2)
+let calcualtorLogic_ = new CalcualtorLogic(6, 5)
+calcualtorLogic_.add(6, 5);
+console.log(calcualtorLogic_)
 
 
-// let result1 = new CalcualtorLogic()
-// result1.add();
-// console.log(result1(1, 7))    // console.log(result.add(1, 2))
-
-// function my(1, 2) {
-//     var a = 2
-//     var b = 3
-//     console.log(a + b)
-
-
-// }
-// my()
-// console.log(a + b)
-
-// function sum(num1, num2) {
-//     var result = num1 + num2
-//     return result
-// }
-// var number1 = 5;
-// var number2 = 10;
-
-// var result1 = sum(number1, number2)
-// console.log(result1)
-
-
-// class CalcualtorLogic {
-
-//     add(number1, number2) {
-
-//         return firstCalculation = number1 + number2;
-//         // return firstCalculation.add(number1, number2);
-//     };
-// };
-// let firstCalculation;
-// // let firstCalculation_ = new CalcualtorLogic(1, 7)
-// firstCalculation.add(1, 2);
-// console.log(firstCalculation.add(1, 2))
 
 // firstCalculation.subtract();
 // firstCalculation.divide();
