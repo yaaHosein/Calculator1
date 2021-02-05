17bae91bda6b2fec60531e781120b2a1e0577e5b
yaaHosein <layali.14.peshawar@hotmail.com>
2021-02-05 16:04:16 +0330

    difining 3 new variables and adding three anoth calculting methods

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -3,7 +3,34 @@ class CalcualtorLogic {
         let result = number1 + number2;
         return result;
     };
+    subtract(number1, number2) {
+        let result = number1 - number2;
+        return result;
+    };
+    multiply(number1, number2) {
+        let result = number1 * number2;
+        return result;
+    };
+    divide(number1, number2) {
+        let result = number1 / number2;
+        return result;
+    };
 };
-let calcualtorLogic = new CalcualtorLogic(1, 2)
-new CalcualtorLogic(1, 2)
-    // new CalcualtorLogic()
+
+let calcualtorLogic = new CalcualtorLogic(1, 2);
+let calcualtorLogic1 = new CalcualtorLogic(1, 2);
+let calcualtorLogic2 = new CalcualtorLogic(1, 2);
+let calcualtorLogic3 = new CalcualtorLogic(1, 2);
+
+
+// let calcualtorLogic = new CalcualtorLogic(1, 2);
+// console.log(calcualtorLogic.add(1, 2))
+
+// let calcualtorLogic1 = new CalcualtorLogic(1, 2);
+// console.log(calcualtorLogic1.subtract(1, 2))
+
+// let calcualtorLogic2 = new CalcualtorLogic(1, 2);
+// console.log(calcualtorLogic2.multiply(1, 2))
+
+// let calcualtorLogic3 = new CalcualtorLogic(1, 2);
+// console.log(calcualtorLogic3.divide(1, 2))
