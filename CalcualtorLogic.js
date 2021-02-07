6490f3312035515c8c10d0b6314e62d7da21b48f
yaaHosein <layali.14.peshawar@hotmail.com>
2021-02-07 07:38:52 +0330

    adding new variable called hostElement in the constructor and adding this keyword to the selector parameter and calling the mentioned variable

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,30 +1,37 @@
-class CalcualtorLogic {
-    add(number1, number2) {
-        let result = number1 + number2;
-        return result;
+{
+    class CalcualtorLogic {
+        add(number1, number2) {
+            let result = number1 + number2;
+            return result;
+        };
+        subtract(number1, number2) {
+            let result = number1 - number2;
+            return result;
+        };
+        multiply(number1, number2) {
+            let result = number1 * number2;
+            return result;
+        };
+        divide(number1, number2) {
+            let result = number1 / number2;
+            return result;
+        };
     };
-    subtract(number1, number2) {
-        let result = number1 - number2;
-        return result;
-    };
-    multiply(number1, number2) {
-        let result = number1 * number2;
-        return result;
-    };
-    divide(number1, number2) {
-        let result = number1 / number2;
-        return result;
-    };
-};
 
-let calcualtorLogic = new CalcualtorLogic();
-calcualtorLogic.add();
-calcualtorLogic.subtract();
-calcualtorLogic.multiply();
-calcualtorLogic.divide();
+    let calcualtorLogic = new CalcualtorLogic();
+    calcualtorLogic.add();
+    calcualtorLogic.subtract();
+    calcualtorLogic.multiply();
+    calcualtorLogic.divide();
 
-class Calcualtor {
-    constructor(selector) {
+    class Calcualtor {
+        constructor(selector) {
+            let hostElement = document.querySelector("#hostElement")
+            this.selector = selector;
+            hostElement()
+        }
 
-    }
-};
+
+    };
+
+}
