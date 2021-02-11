67fb08eedf8cd23f7120e728d86934413ee3dada
yaaHosein <layali.14.peshawar@hotmail.com>
2021-02-11 23:45:26 +0330

    trying to show bold calculator word in the doc

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -1,39 +1,43 @@
-{
-    class CalcualtorLogic {
-        add(number1, number2) {
-            let result = number1 + number2;
-            return result;
-        };
-        subtract(number1, number2) {
-            let result = number1 - number2;
-            return result;
-        };
-        multiply(number1, number2) {
-            let result = number1 * number2;
-            return result;
-        };
-        divide(number1, number2) {
-            let result = number1 / number2;
-            return result;
-        };
+class CalcualtorLogic {
+    add(number1, number2) {
+        let result = number1 + number2;
+        return result;
     };
+    subtract(number1, number2) {
+        let result = number1 - number2;
+        return result;
+    };
+    multiply(number1, number2) {
+        let result = number1 * number2;
+        return result;
+    };
+    divide(number1, number2) {
+        let result = number1 / number2;
+        return result;
+    };
+};
 
-    let calcualtorLogic = new CalcualtorLogic();
-    calcualtorLogic.add();
-    calcualtorLogic.subtract();
-    calcualtorLogic.multiply();
-    calcualtorLogic.divide();
+let calcualtorLogic = new CalcualtorLogic();
+calcualtorLogic.add();
+calcualtorLogic.subtract();
+calcualtorLogic.multiply();
+calcualtorLogic.divide();
 
-    class Calcualtor {
-        constructor(selector) {
-            this.selector = selector;
-            // let buttons = document.querySelectorAll(".button")
-            let hostElem = document.querySelector("#hostElement").innerHTML = "I love Sayed Mohammad Hassan💖🌹 and His beautiful and sweet daughter 💖Liza💖"
-            console.log(hostElem);
-        };
-    }
-
-
-    // let calcualtor = new Calcualtor();
-
+class Calculator {
+    constructor(selector) {
+        // this.selector = document.querySelectorAll(".button")
+        this.selector = document.querySelector(selector);
+        this.selector.innerHTML = `
+            <div>
+                <h2>
+                    Calcualtor
+                </h2>
+            </div>
+            `
+    };
 }
+const calc1 = new Calculator(".foo");
+const calc2 = new Calculator("#baz");
+const calc3 = new Calculator("#hostElement");
+
+// let calcualtor = new Calcualtor(".button");
