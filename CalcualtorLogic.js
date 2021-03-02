558689b3b659f05575d096d96ccc6fbd65628bec
yaaHosein <layali.14.peshawar@hotmail.com>
2021-03-02 14:33:54 +0330

    removing the these codes:   if (buttonElement === Operator) { resultElement.textContent =  };  from getButtonsRow method

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -79,6 +79,7 @@ class Calculator {
             buttonElement.appendChild(buttonElementText);
             div.appendChild(buttonElement);
             buttonElement.addEventListener("click", this.onButtonNumberClick);
+
         }
         return div;
     }
