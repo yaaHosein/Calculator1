fec7dcdb2aca7db33ae3d7349643496bebb9751f
yaaHosein <layali.14.peshawar@hotmail.com>
2021-03-13 18:02:43 +0330

    changing operator propriety's value to null

--- CalcualtorLogic.js
+++ CalcualtorLogic.js
@@ -26,7 +26,7 @@ calculatorLogic.divide();
 class Calculator {
     currentResult = 0;
     resultElement = null;
-    operator = this.buttonElement;
+    operator = null;
     constructor(selector) {
         this.hostElem = document.querySelector(selector);
         this.onButtonNumberClick = this.onButtonNumberClick.bind(this);
