41af373444f2aab13c8cf244406a3fdeb6c5ec3c
yaaHosein <layali.14.peshawar@hotmail.com>
2021-04-12 23:26:38 +0430

    hosting the developed calculator

--- develpoedCalculator.js
+++ develpoedCalculator.js
@@ -1,1 +1,8 @@
-class DevelopedCalculator extends Calculator {};
+class DevelopedCalculator extends Calculator {
+    constructor(selector) {
+        this.hostElem1 = document.querySelector(selector);
+    }
+
+}
+let developedCalculator = new DevelopedCalculator("#hostElem1");
+developedCalculator.render();
