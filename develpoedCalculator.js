f8689726a74b32230bb27831cff14d4d0dc7ea99
yaaHosein <layali.14.peshawar@hotmail.com>
2021-04-13 18:42:59 +0430

    removing the structor function

--- develpoedCalculator.js
+++ develpoedCalculator.js
@@ -1,7 +1,5 @@
 class DevelopedCalculator extends Calculator {
-    constructor(selector) {
-        this.hostElem1 = document.querySelector(selector);
-    }
-};
-let developedCalculator = new DevelopedCalculator("#hostElement_");
-developedCalculator.render();
+
+}
+const developedCalculator = new DevelopedCalculator("#hostElement_");
+developedCalculator.render()
