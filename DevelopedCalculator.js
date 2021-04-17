1ad37f3e4ae4389805b96b3c0b7738acab35fe6c
yaaHosein <layali.14.peshawar@hotmail.com>
2021-04-17 21:36:13 +0430

    adding three variables to render method

--- DevelopedCalculator.js
+++ DevelopedCalculator.js
@@ -1,6 +1,9 @@
 class DevelopedCalculator extends Calculator {
     render() {
         super.render()
+        const history = document.createElement('div');
+        const historyDiv_ = document.createTextNode("History:");
+        const historyDiv = this.hostElm.prepend(historyDiv)
     }
 }
 
