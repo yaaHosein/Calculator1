b68d95ad3f979448b02b6e39b58d5860d5724c1f
yaaHosein <layali.14.peshawar@hotmail.com>
2021-04-18 14:40:07 +0430

    correcting the way of adding text to the element

--- DevelopedCalculator.js
+++ DevelopedCalculator.js
@@ -1,11 +1,12 @@
 class DevelopedCalculator extends Calculator {
     render() {
         super.render()
+            // first create a div 
+        const historyDiv = document.createElement("div");
+        // add the text to the div
+        historyDiv.appendChild(document.createTextNode("History:"));
+        // prepend the historyDiv in the host element 
         this.hostElm.prepend(historyDiv);
-        const historyDiv = document.createElement('div');
-        const historyDivText = document.createTextNode("History:");
-        historyDiv_.appendChild(historyDivText);
-        historyDiv.appendChild(historyDiv_);
     }
 }
 
