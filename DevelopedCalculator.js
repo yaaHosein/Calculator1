e0e668cb0100a3758e10e455b2fc64629bb4d2cc
yaaHosein <layali.14.peshawar@hotmail.com>
2021-04-19 14:06:27 +0430

    adding current result to the History and updating the result

--- DevelopedCalculator.js
+++ DevelopedCalculator.js
@@ -4,9 +4,13 @@ class DevelopedCalculator extends Calculator {
             // first create a div 
         const historyDiv = document.createElement("div");
         // add the text to the div
-        historyDiv.appendChild(document.createTextNode("History:"));
+        historyDiv.appendChild(document.createTextNode("History:" + this.currentResult));
         // prepend the historyDiv in the host element 
         this.hostElem.prepend(historyDiv);
+
+
+        this.updateResultElement();
+
     }
 }
 const developedCalculator = new DevelopedCalculator("#hostElement_");
