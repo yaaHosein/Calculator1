3978714a1d18565b786a0d4adc02cae2d875a8e3
yaaHosein <layali.14.peshawar@hotmail.com>
2021-05-18 12:29:51 +0430

    in onOperatorButtonClick meth defining var and puhing it and unactivating the pushing inside onNumberButtonClick

--- DevelopedCalculator.js
+++ DevelopedCalculator.js
@@ -4,6 +4,7 @@ class DevelopedCalculator extends Calculator {
     history = [this.currentResult];
     // saving historyElement as a property on the class
     historyElement = null;
+
     render() {
             //calling the render method of the parent class
             super.render()
@@ -16,33 +17,29 @@ class DevelopedCalculator extends Calculator {
         }
         // new method has a parameter
     onButtonNumberClick(event) {
-        // calling parent class's onButtonNumberClick method
+        // calling parent class's onButtonNumberClick method1
         super.onButtonNumberClick(event);
         // pushing the currentResult to the history array 
-        this.history.push(this.currentResult);
+        // this.history.push(this.currentResult);
         // calling gettingHistoryArray to update the history element after clicking any number button 
         this.gettingHistoryArray();
     }
     gettingHistoryArray() {
         // updating the history element by the history which is joined with ,
-        //  this.historyElement.textContent = this.history.join(",");
-        // updating the history element by the history which is joined with ""
-        this.historyElement.textContent = this.history.join("");
+        this.historyElement.textContent = this.history.join(",")
     };
-
     onOperatorButtonClick(event) {
-        // calling parent class's onOperatorButtonClick method
-        super.onOperatorButtonClick(event);
-        // pushing the operator to the history array 
-        this.history.push(this.operator);
-        // calling gettingHistoryArray to update the history element after clicking any operator button 
-        this.gettingHistoryArray();
-    }
-    onClearButtonClick() {
-        super.onClearButtonClick()
-    }
+            // calling parent class's onOperatorButtonClick method
+            super.onOperatorButtonClick(event);
+            const curntresultandopr = this.currentResult + this.operator
+            this.history.push(curntresultandopr);
+            // calling gettingHistoryArray to update the history element after clicking any operator button 
+            this.gettingHistoryArray();
+        }
+        //onClearButtonClick() {
+        //    super.onClearButtonClick()
+        // }
 }
-
 // defining instance 
 const developedCalculator = new DevelopedCalculator("#hostElement_");
 // calling the render method      
